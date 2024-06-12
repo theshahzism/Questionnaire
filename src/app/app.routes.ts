316ff6878dce0aef25edbf2a9dc43cf6818b6d16
P5Cure <hello@p5cure.com>
2024-06-12 16:59:38 +0500

    Import remains for angular material input

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { QuestionComponent } from './pages/question/question.component';
+import { QuestionInfoComponent } from './pages/question-info/question-info.component';
+import { QuestionJoinComponent } from './pages/question-join/question-join.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path:"",
+    component:QuestionJoinComponent
+    },
+    {
+    path:"question-info",
+    component:QuestionInfoComponent
+    },
+    {
+    path:"question",
+    component:QuestionComponent
+    }
+];
